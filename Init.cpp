9a3a5624ef8ca391e9baa3d528bd98af639da5e3
Do-Minh-Triet <n24dece046@student.ptithcm.edu.vn>
2026-07-12 10:57:50 +0700

    khai bao lan dau

--- Init.cpp
+++ Init.cpp
@@ -9,6 +9,16 @@ struct MonHoc{
 
 // BST cho mon hoc ( kem cac ham phu tro insert , search , delete )
 
+struct NodeMH{
+    MonHoc monhoc;
+    NodeMH* left;
+    NodeMH* right;
+    NodeMH(MonHoc monhoc){
+        left = nullptr;
+        right = nullptr;
+        monhoc = monhoc;
+    }
+};
 
 struct CauHoi{
     int ID; // ID tu dong tang , khong trung lap
@@ -26,3 +36,54 @@ struct dsCHT{
     
 };
 
+struct DiemThi{
+    string MAMH;
+    float DIEM;
+};
+
+struct dsDiemThi{
+    dsDiemThi* next;
+    DiemThi diemthi;
+    dsDiemThi(DiemThi diemthi){
+        next = nullptr;
+        diemthi = diemthi;
+    }
+};
+
+struct SinhVien{
+    string MASV;
+    string HO;
+    string TEN;
+    string PHAI;
+    string passsword;
+    dsDiemThi* dsdiemthi = NULL;
+};
+
+struct dsSinhVien{
+    dsSinhVien* next;
+    SinhVien sinhvien;
+    dsSinhVien(SinhVien sinhvien){
+        next = nullptr;
+        sinhvien = sinhvien;
+    }
+};
+
+
+const int MAXLOP =10000;
+struct Lop{
+    string MALOP;
+    string TENLOP;
+    dsSinhVien* dssinhvien = NULL;
+};
+
+struct dsLop{
+    int n;
+    Lop* lop[MAXLOP];
+};
+
+
+int main(){
+    dsLop* dslop = new dsLop();
+    NodeMH* root = nullptr;
+    
+}
